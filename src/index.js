52f9fa5ad574097141301f9fe7f093f4dcdf7635
QuanKHs <quanphamluong@gmail.com>
2020-12-24 15:42:45 +0700

    creat mvc model

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 //Template engine
 app.engine('hbs', handlebars({
@@ -13,11 +16,10 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
+//route init
+route(app);
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
